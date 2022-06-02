161142aa696d15e153056eb3a582616d7be7bd18
Ana Tavares <ana.tavares@smartdata.science>
2022-06-02 13:14:07 -0300

    Added TweetBox

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,12 +1,17 @@
-import { RefreshIcon } from "@heroicons/react/outline"
+import { RefreshIcon } from '@heroicons/react/outline'
+import TweetBox from './TweetBox'
 
 export default function Feed() {
   return (
-    <div className="col-span-7 lg:col-span-5">
-        <header className="flex items-center justify-between">
-            <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-            <RefreshIcon className="mr-5 mt-5 w-8 h-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
-        </header>
+    <div className="col-span-7 border-x lg:col-span-5">
+      <header className="flex items-center justify-between">
+        <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
+        <RefreshIcon className="mr-5 mt-5 w-8 h-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+      </header>
+
+      <div>
+        <TweetBox/>
+      </div>
     </div>
   )
 }
